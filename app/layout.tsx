2666ae0681263a8ef482119d1b7963f4fc508bfb
ZainabShujat <zainabshujatali@gmail.com>
2026-01-10 18:58:49 +0530

    added googel tag manager

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,6 +20,15 @@ export default function RootLayout({
   return (
     <html lang="en" className="antialiased scroll-smooth">
       <head>
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MF94KHX2');
+        `}} />
+        {/* End Google Tag Manager */}
         {/* <!-- Primary Meta Tags --> */}
 <title>Zainab Shujat — Creative Developer</title>
 <meta name="title" content="Zainab Shujat — Creative Developer" />
